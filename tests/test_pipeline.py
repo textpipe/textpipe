ef3fad9d13ebae518b5fb072c8af3982b85f37c6
Tanja Crijns <tanja.crijns@rtl.nl>
2018-02-20 15:13:11 +0100

    Add pipeline class functionality and tests

---
+++ tests/test_pipeline.py
@@ -0,0 +1,17 @@
+"""
+Testing for textpipe pipeline.py
+"""
+
+from textpipe.textpipe.pipeline import Pipeline
+
+TEXT = 'Test sentence for testing'
+PIPELINE = ['raw', 'nwords', 'complexity', 'clean_text']
+PIPE = Pipeline(PIPELINE)
+
+
+def test_return_dict():
+    """
+    The returned dictionary should have the same length as the pipeline
+    """
+    assert len(PIPE(TEXT)) == len(PIPELINE)
+
