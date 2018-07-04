821e709b183c296e3cd00dfd4fae73aca980e04e
graus <david.graus@fdmediagroep.nl>
2018-07-04 16:32:38 +0200

    Adds pipeline serialization using  json, also turns parameters into keyword arguments

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -14,3 +14,16 @@ def test_return_dict():
     The returned dictionary should have the same length as the pipeline
     """
     assert len(PIPE(TEXT)) == len(PIPELINE)
+
+
+def test_load_save():
+    """
+    The property of saves model should be the same as the loaded model after serialization.
+    """
+
+    PIPE.language = 'it'
+    PIPE.save('test.pkl')
+
+    p = Pipeline.load('test.pkl')
+
+    assert p.language == PIPE.language
