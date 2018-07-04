ea823e84bdce2f6e3877f81b34471e70e6d5acb1
graus <david.graus@fdmediagroep.nl>
2018-07-04 16:32:38 +0200

    Adds pipeline serialization using  json, also turns parameters into keyword arguments

--- textpipe/pipeline.py
+++ textpipe/pipeline.py
@@ -3,6 +3,7 @@ Obtain elements from a textpipe doc, by specifying a pipeline, in a dictionary.
 """
 
 from textpipe.doc import Doc
+import json
 
 
 class Pipeline:
@@ -13,7 +14,7 @@ class Pipeline:
     >>> sorted(pipe('Test sentence <a=>').items())
     [('clean_text', 'Test sentence '), ('nwords', 2), ('raw', 'Test sentence <a=>')]
     """
-    def __init__(self, pipeline, language=None, hint_language='en'):
+    def __init__(self, **kwargs):
         """
         Initialize a Pipeline instance
 
@@ -22,9 +23,11 @@ class Pipeline:
         language: 2-letter code for the language of the text
         hint_language: language you expect your text to be
         """
-        self.pipeline = pipeline
-        self.language = language
-        self.hint_language = hint_language
+        self.pipeline = kwargs.get('pipeline', None)
+        self.language = kwargs.get('language', None)
+        self.hint_language = kwargs.get('hint_language', None)
+
+
 
     def __call__(self, raw):
         """
@@ -37,3 +40,26 @@ class Pipeline:
         doc = Doc(raw, language=self.language, hint_language=self.hint_language)
         result_dict = dict(zip(self.pipeline, [doc.__getattribute__(x) for x in self.pipeline]))
         return result_dict
+
+    def save(self, filename):
+        """
+        Serializes the pipeline to file (using pickle)
+
+        Args:
+        filename: location of where to serialize pipeline
+        """
+
+        with open(filename, 'w') as json_file:
+            json.dump(self.__dict__, json_file)
+
+    @staticmethod
+    def load(filename):
+        """
+        Loads pipeline from serialized file
+
+        Args:
+        filename: location of pickled Pipeline object
+        """
+
+        with open(filename, 'r') as json_file:  # initialize
+            return Pipeline(**json.load(json_file))
