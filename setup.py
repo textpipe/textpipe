769ec85ba3c6a22e94a778ab8d873368a78188e1
Daan Odijk <daan.odijk@rtl.nl>
2018-08-28 10:32:52 +0200

    Consistently use ' instead of " in setup.py

--- setup.py
+++ setup.py
@@ -14,15 +14,15 @@ with open(Path(__file__).resolve().parent.joinpath('VERSION'), 'r') as fh:
 setuptools.setup(
     name='textpipe',
     version=version,
-    description="textpipe: clean and extract metadata from text",
+    description='textpipe: clean and extract metadata from text',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/textpipe/textpipe",
+    long_description_content_type='text/markdown',
+    url='https://github.com/textpipe/textpipe',
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3.6",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
     install_requires=requirements,
 )
