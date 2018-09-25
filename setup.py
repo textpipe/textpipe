1811711adf516c1edd2d0564362a727bc24065c9
Anne Schuth <anne.schuth@persgroep.net>
2018-09-25 13:01:35 +0200

    We added our pattern version to pypi

--- setup.py
+++ setup.py
@@ -24,6 +24,5 @@ setuptools.setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    install_requires=[r for r in requirements if not r.startswith('git+https://')],
-    dependency_links=[f'{r[4:-4]}/tarball/master#egg=package-1.0' for r in requirements if r.startswith('git+https://')],
+    install_requires=requirements,
 )
