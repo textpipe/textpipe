2539b0a69b9a6c19592b833121a8a90932fbc18e
Lucas de Haas <lmdehaas@gmail.com>
2018-10-31 16:06:05 +0100

    Reverts rename of exception

--- textpipe/doc.py
+++ textpipe/doc.py
@@ -14,7 +14,7 @@ import textacy.text_utils
 from bs4 import BeautifulSoup
 
 
-class MissingModelException(Exception):
+class TextpipeMissingModelException(Exception):
     """Raised when the requested model is missing"""
     pass
 
@@ -127,7 +127,7 @@ class Doc:
                                             model_name not in self._spacy_nlps[lang]):
             self._set_default_nlp(lang)
         if model_name not in self._spacy_nlps[lang] and model_name is not None:
-            raise MissingModelException(f'Custom model {model_name} '
+            raise TextpipeMissingModelException(f'Custom model {model_name} '
                                                 f'is missing.')
         nlp = self._spacy_nlps[lang][model_name]
         doc = nlp(self.clean_text())
@@ -289,7 +289,7 @@ class Doc:
         try:
             model = spacy.load('{}_core_{}_sm'.format(lang, 'web' if lang == 'en' else 'news'))
         except IOError:
-            raise MissingModelException(f'Default model for language "{lang}" is not available.')
+            raise TextpipeMissingModelException(f'Default model for language "{lang}" is not available.')
         self._spacy_nlps[lang][None] = model
 
     @property
@@ -317,7 +317,7 @@ class Doc:
             from pattern.text.it import sentiment as sentiment_it
             return sentiment_it(self.clean)
 
-        raise MissingModelException(f'No sentiment model for {self.language}')
+        raise TextpipeMissingModelException(f'No sentiment model for {self.language}')
 
     @functools.lru_cache()
     def extract_keyterms(self, ranker='textrank', n_terms=10, **kwargs):
