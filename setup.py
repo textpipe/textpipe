e53b60e58e673946c036e31532f7f7edb90c734a
Anne Schuth <anne.schuth@persgroep.net>
2019-04-01 16:55:11 +0200

    Adds python 3.7 to setup.py

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
