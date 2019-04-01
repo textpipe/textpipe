f2e72efa5ad376a280ff52e2836f4136fce87050
Lucas de Haas <lmdehaas@users.noreply.github.com>
2019-04-01 14:59:00 +0200

    Update textpipe/doc.py
    Co-Authored-By: anneschuth <anne.schuth@gmail.com>

--- textpipe/doc.py
+++ textpipe/doc.py
@@ -102,7 +102,7 @@ class Doc:
         """
 
         none_utf_chars_removed = ''.join([l for l in self.clean
-                                          if unicodedata.category(l)[0] not in ('M', 'C')])
+                                          if unicodedata.category(l)[0] not in {'M', 'C'}])
         is_reliable, _, best_guesses = cld2.detect(none_utf_chars_removed,
                                                    hintLanguage=hint_language,
                                                    bestEffort=True)
