e7ce50ad421c09f8575ce1394d060c80cfc71621
Anne Schuth <anne.schuth@persgroep.net>
2019-07-18 11:47:55 +0200

    fixes pylint errors in wrappers.py

--- textpipe/wrappers.py
+++ textpipe/wrappers.py
@@ -15,14 +15,14 @@ from tqdm import tqdm
 
 class RedisKeyedVectorException(Exception):
     """ Raised when RedisKeyedVectors class fails"""
-    pass
 
 
 class RedisKeyedVectors:
     """
     Class to imitate gensim's KeyedVectors, but instead getting the vectors from the memory,
     the vectors will be retrieved from a redis db.
-    Based on https://engineering.talentpair.com/serving-word-vectors-for-distributed-computations-c5065cbaa02f
+    Based on https://engineering.talentpair.com/serving-word-vectors-
+    for-distributed-computations-c5065cbaa02f
     """
 
     def __init__(self, uri, key='', max_lru_cache_size=1024):
@@ -30,13 +30,14 @@ class RedisKeyedVectors:
         self.key = f'w2v_{key}'
 
         try:
-            host, port, db = self._parse_uri(uri)
-            self._redis = Redis(host, port, db)
-        except RedisError as e:
+            host, port, database = self._parse_uri(uri)
+            self._redis = Redis(host, port, database)
+        except RedisError as exception:
             raise RedisKeyedVectorException(f'The connection to Redis failed while trying to '
-                                            f'initiate the client. Redis error message: {e}')
+                                            f'initiate the client. Redis error message: '
+                                            f'{exception}')
 
-    def word_vec(self, word):
+    def word_vec(self, word):  # pylint: disable=E0202
         """
         This method is mimicking the word_vec method from the Gensim KeyedVector class. Instead of
         looking it up from an in memory dict, it
@@ -55,26 +56,30 @@ class RedisKeyedVectors:
             if not cache_entry:
                 raise KeyError(f'Key {word} does not exist in cache')
             return pickle.loads(cache_entry)
-        except RedisError as e:
+        except RedisError as exception:
             raise RedisKeyedVectorException(f'The connection to Redis failed while trying to '
-                                            f'retrieve a word vector. Redis error message: {e}')
+                                            f'retrieve a word vector. Redis error message: '
+                                            f'{exception}')
         except TypeError:
             return None
 
     def __getitem__(self, words):
         """
-        returns numpy array for single word or vstack for multiple words
+        Returns numpy array for single word or vstack for multiple words
         """
         if isinstance(words, str):
             return self.word_vec(words)
         return np.vstack([self.word_vec(word) for word in words])
 
     def __contains__(self, word):
-        """ build in method to quickly check whether a word is available in redis """
+        """
+        build in method to quickly check whether a word is available in redis
+        """
         return self._redis.hexists(self.key, word)
 
     def load_keyed_vectors_into_redis(self, model_path):
-        """ This function loops over all available words in the loaded word2vec keyed vectors model
+        """
+        This function loops over all available words in the loaded word2vec keyed vectors model
         and loads them into the redis instance.
         """
         model = KeyedVectors.load(model_path, mmap='r')
@@ -82,13 +87,16 @@ class RedisKeyedVectors:
             for word in tqdm(list(model.vocab.keys())):
                 idf_normalized_vector = model[word] / model.vocab[word].count
                 self._redis.hset(self.key, word, pickle.dumps(idf_normalized_vector))
-        except RedisError as e:
+        except RedisError as exception:
             raise RedisKeyedVectorException(f'RedisError while trying to load model {model} '
-                                            f'into redis: {e}')
+                                            f'into redis: {exception}')
         del model
 
     @property
     def exists(self):
+        """
+        Indicates whether the key exists.
+        """
         return bool(self._redis.exists(self.key))
 
     @staticmethod
@@ -97,7 +105,7 @@ class RedisKeyedVectors:
         try:
             host = parsed.hostname
             port = parsed.port
-            db = int(parsed.path.replace('/', '')) or 0
+            database = int(parsed.path.replace('/', '')) or 0
         except (AttributeError, ValueError):
             raise RedisKeyedVectorException(f'Invalid redis URI: {uri}')
-        return host, port, db
+        return host, port, database
